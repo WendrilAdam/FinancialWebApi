4108ce8eda3c0c0975a3ee90f0a3ce9af0588e9f
Wendril Adam <adamwendril@gmail.com>
2020-10-30 14:53:23 -0300

    Adicionado swagger e pasta models

--- Project/Controllers/WeatherForecastController.cs
+++ Project/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Project.Controllers
+namespace FinancialWebApi.Project.Controllers
 {
     [ApiController]
     [Route("[controller]")]
