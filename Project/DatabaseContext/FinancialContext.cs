c650e7a346b3c33dccc1d6ea85df3791f3092524
Wendril Adam <adamwendril@gmail.com>
2020-11-02 00:39:34 -0300

    Engenharia reversa do banco de dados financial

---
+++ Project/DatabaseContext/FinancialContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using FinancialWebApi.Project.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FinancialWebApi.Project.DatabaseContext
+{
+    public partial class FinancialContext : DbContext
+    {
+        public FinancialContext()
+        {
+        }
+
+        public FinancialContext(DbContextOptions<FinancialContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=CASA-PC\\SQLEXPRESS,1433;Initial Catalog=Financial;User ID=sa;Password=10081997w;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Cpf)
+                    .IsRequired()
+                    .HasColumnName("CPF")
+                    .HasMaxLength(14);
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.NomeCompleto)
+                    .IsRequired()
+                    .HasMaxLength(250);
+
+                entity.Property(e => e.TelefoneCelular).HasMaxLength(20);
+
+                entity.Property(e => e.TelefoneFixo).HasMaxLength(20);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- Project/Models/WeatherForecast.cs
+++ Project/Models/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace FinancialWebApi.Project
+namespace FinancialWebApi.Project.Models
 {
     public class WeatherForecast
     {
